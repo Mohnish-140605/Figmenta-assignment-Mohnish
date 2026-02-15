a6d35fdca90feb5e89fc6dbc6cd1bda7d5a44af7
Bharath200027 <mohnishfit05@gmail.com>
2026-02-15 22:57:19 +0530

    Fix Netlify 502 error: remove custom install script

--- api/debug.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-    res.status(200).json({
-        message: 'Debug endpoint is working!',
-        timestamp: new Date().toISOString(),
-        headers: req.headers,
-        url: req.url
-    });
-};
--- netlify/functions/api.js
+++
@@ -1,4 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../../api/app'); // Import from api directory
-
-module.exports.handler = serverless(app);
